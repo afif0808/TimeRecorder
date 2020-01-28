d6c0d960b27caea73bb5f3cde828a9c726488c8f
afif0808 <afif.panai@gmail.com>
2020-01-28 08:03:30 +0700

    Just Another Commit

--- app/src/main/java/com/example/timerecorder/MainActivity.java
+++ app/src/main/java/com/example/timerecorder/MainActivity.java
@@ -25,17 +25,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        TimeRecorder timeRecord = new TimeRecorder("Just Another Time Record");
-        timeRecord.startRecordingTime();
-        Log.d("just another log", String.valueOf(timeRecord.getTimeInDays()));
 
     }
 
